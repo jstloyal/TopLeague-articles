bd1455cc9098d0c4a4524bc42461544c87065b6f
jstloyal <jstloyalty@gmail.com>
2020-08-08 13:01:19 +0100

    Create articles new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,25 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created!"
+      redirect_to article_path(@article)
+    else
+      flash[:alert] = "An error occured"
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text, :image )
+  end
 end
