aa2c2d5dc7f4e227007ce712cfeaa83aaae43242
jstloyal <jstloyalty@gmail.com>
2020-08-12 05:27:23 +0100

    Add model tests

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:article_categories) }
+    ir { should have_many(:articles).through(:article_categories) }
+  end
+end
