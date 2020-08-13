66205544a5d67ebe829de42c6f800776ee3746a2
jstloyal <jstloyalty@gmail.com>
2020-08-13 15:32:28 +0100

    Solve N+1 problem using bullet gem

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   # before_action :require_user, only: [:show]
 
   def index
-    @categories = Category.all
+    # @categories = Category.all
     # @liked_article = Article.highest_vote.first
     @liked_article = Article.all
   end
