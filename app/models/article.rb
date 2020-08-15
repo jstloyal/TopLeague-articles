422651a87d5c450b0178dcabcf7d35971526a376
jstloyal <jstloyalty@gmail.com>
2020-08-15 08:42:15 +0100

    Add stickler file

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }
   validates :text, presence: true, length: { minimum: 10, maximum: 5000 }
-  validates_presence_of :image, message: 'image not selected!'
+  # validates :image, attached: true, content_type: %w[jpg jpeg gif png]
 
   belongs_to :author, class_name: 'User'
   has_many :article_categories
