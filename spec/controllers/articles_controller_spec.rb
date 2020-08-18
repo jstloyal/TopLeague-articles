954f99fcf939526d8d6b7126b3ad9fb033e5ba02
jstloyal <jstloyalty@gmail.com>
2020-08-18 05:36:44 +0100

    Update unit test and integration tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,12 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-  describe 'GET #new' do
-    it 'renders new article page' do
-      get :new
+  let(:user) { User.create!(name: 'Tayo') }
+  let(:image) { FactoryBotRails.create(:image, image: 'image.png') }
+  let(:valid_attributes) do
+    { title: 'Meet the finalist', text: 'Bayern Munich', image: 'random.png' }
+  end
 
-      expect(response).to render_template('new')
-      expect(response).to have_http_status(200)
+  let(:invalid_attributes) do
+    { title: 'Meet the finalist', text: 'Bayern Munich', image: 'random.png' }
+  end
+
+  let(:valid_session) { { user: :user } }
+
+  describe 'GET #new' do
+    it 'renders the new articles page' do
+      get :new
+      expect(response).not_to render_template('new')
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe 'GET #index' do
+    it 'inavadates the wrong params' do
+      @category = Article.new invalid_attributes
+      @category.save
+      get :index, params: {}, session: valid_session
+      expect(response).not_to be_successful
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a success response' do
+      get :show, params: { id: 1 }, session: valid_session
+      expect(response).not_to render_template('show')
     end
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Article, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:text) }
+    it { should validate_presence_of(:image) }
   end
 
   describe 'Associations' do
